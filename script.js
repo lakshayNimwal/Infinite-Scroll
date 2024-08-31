62fd3567c31422b41d49e87406e6e93eda9ac32d
Lakshay <1100lakshaynimwal@gmail.com>
2024-09-01 00:02:15 +0530

    second:display photos on ui

--- script.js
+++ script.js
@@ -2,25 +2,52 @@
 // 649087
 
 const count = 10;
-const apiKey = '_PiVIODJVm-MuDmtDPXCEqI8BkFr0HyDq4Zv1kCvhwQ';
+const apiKey = "_PiVIODJVm-MuDmtDPXCEqI8BkFr0HyDq4Zv1kCvhwQ";
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
 
-// getting photos from Unsplash API
-async function getPhotos() {
-     try{
-      const response = await fetch(apiUrl);
-      const data = await response.json()
-      console.log(data);
-      
+let photosArray = [];
 
-     }catch (error){
-      // error
-      console.log(error);
-      
+// create elements for Links & Photos, Add to DOM
+function displayPhotos(){
+      // Run function for each object in array 
+      photosArray.forEach((photos) => {
+            // Create <a> to link to unsplash
+            const item = document.createElement('a');
+            item.setAttribute('href',photos.links.html);
+            item.setAttribute('target', '_blank');
 
-     }
+            //create img element
+            const img = document.createElement('img');
+            img.setAttribute('src', photos.urls.regular)
+            img.setAttribute('alt', photos.alt_description);
+            img.setAttribute('title', photos.alt_description);
+
+            // Put <a> inside <img> then put both inside image container
+            item.appendChild(img);
+            imageContainer.appendChild(item);
+            
+      });
       
       
 }
 
-getPhotos()
+
+// getting photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    console.log(photosArray[0]);
+    
+    // disply photos function
+    displayPhotos();
+  } catch (error) {
+    // error
+    console.log(error);
+  }
+}
+
+// on Load
+getPhotos();
